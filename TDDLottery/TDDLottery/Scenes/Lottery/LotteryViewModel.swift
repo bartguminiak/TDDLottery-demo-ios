0ae14e289dbac51cbec475534f745511aac15c4b
Bartlomiej <guminiak.bartlomiej@gmail.com>
2019-05-29 18:17:31 +0200

    Add logic

--- TDDLottery/TDDLottery/Scenes/Lottery/LotteryViewModel.swift
+++ TDDLottery/TDDLottery/Scenes/Lottery/LotteryViewModel.swift
@@ -1,16 +1,29 @@
-protocol LotteryViewModelType: class {
+protocol LotteryViewModelType {
     func viewDidLoad()
     var circleViewModels: (([CircleViewModelType]) -> Void)? { get set }
 }
 
 class LotteryViewModel: LotteryViewModelType {
 
+    init(numberGenerator: NumberGeneratorType,
+         circleViewModelFactory: @escaping (Int) -> CircleViewModelType) {
+        self.numberGenerator = numberGenerator
+        self.circleViewModelFactory = circleViewModelFactory
+    }
+
     // MARK: - LotteryViewModelType
 
     func viewDidLoad() {
-        //
+        let numbers = numberGenerator.generate()
+        let viewModels = numbers.map { circleViewModelFactory($0) }
+        circleViewModels?(viewModels)
     }
 
     var circleViewModels: (([CircleViewModelType]) -> Void)?
+
+    // MARK: - Privates
+
+    private let numberGenerator: NumberGeneratorType
+    private let circleViewModelFactory: (Int) -> CircleViewModelType
 
 }
