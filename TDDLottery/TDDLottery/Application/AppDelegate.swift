5326fd922e8f8f50c9772804764391f968c6af14
Bartlomiej <guminiak.bartlomiej@gmail.com>
2019-05-29 15:56:44 +0200

    Add dependencies and logic

--- TDDLottery/TDDLottery/Application/AppDelegate.swift
+++ TDDLottery/TDDLottery/Application/AppDelegate.swift
@@ -3,11 +3,23 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    override init() {
+        self.container = Container()
+        self.appConfigurator = container.appConfigurator()
+        super.init()
+    }
+
     var window: UIWindow?
+    var appConfigurator: AppConfiguratorType
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = appConfigurator.configuredWindow()
         return true
     }
+
+    // MARK: - Privates
+
+    private let container: Container
 
 }
 
