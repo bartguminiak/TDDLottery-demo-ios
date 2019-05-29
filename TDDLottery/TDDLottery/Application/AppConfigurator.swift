a88d9e996164c046cb2abe43b66121f268d29918
Bartlomiej <guminiak.bartlomiej@gmail.com>
2019-05-29 14:51:09 +0200

    Update logic

--- TDDLottery/TDDLottery/Application/AppConfigurator.swift
+++ TDDLottery/TDDLottery/Application/AppConfigurator.swift
@@ -6,18 +6,24 @@ protocol AppConfiguratorType {
 
 class AppConfigurator: AppConfiguratorType {
 
-    init(windowFactory: @escaping () -> UIWindow) {
+    init(windowFactory: @escaping () -> UIWindow,
+         rootViewControllerFactory: @escaping () -> UIViewController) {
         self.windowFactory = windowFactory
+        self.rootViewControllerFactory = rootViewControllerFactory
     }
 
     // MARK: - AppConfiguratorType
 
     func configuredWindow() -> UIWindow {
-        fatalError()
+        let window = windowFactory()
+        window.rootViewController = rootViewControllerFactory()
+        window.makeKeyAndVisible()
+        return window
     }
 
     // MARK: - Privates
 
     private let windowFactory: () -> UIWindow
+    private let rootViewControllerFactory: () -> UIViewController
 
 }
