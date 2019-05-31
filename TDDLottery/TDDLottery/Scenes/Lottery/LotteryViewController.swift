509d6f654f545dc915b2db43108e29eac32ac5f4
Bartlomiej <guminiak.bartlomiej@gmail.com>
2019-05-31 13:59:21 +0200

    Add logic

--- TDDLottery/TDDLottery/Scenes/Lottery/LotteryViewController.swift
+++ TDDLottery/TDDLottery/Scenes/Lottery/LotteryViewController.swift
@@ -9,13 +9,34 @@ class LotteryViewController: UIViewController {
 
     required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
 
+    override func loadView() {
+        view = LotteryView()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureSubviews()
     }
 
     // MARK: - Privates
 
     private let viewModel: LotteryViewModelType
+    private var lotteryView: LotteryView! {
+        return view as? LotteryView
+    }
+
+    private func configureSubviews() {
+        viewModel.circleViewModels = { [unowned self] viewModels in
+            let labels = viewModels.map { viewModel -> UILabel in
+                let label = UILabel()
+                label.text = viewModel.title
+                label.backgroundColor = viewModel.color
+                return label
+            }
+            labels.forEach(self.lotteryView.stackView.addArrangedSubview)
+        }
+        viewModel.viewDidLoad()
+    }
 
 }
 
