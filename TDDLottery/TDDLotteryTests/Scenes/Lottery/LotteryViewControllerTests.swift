f97ca612d24901ef1eea856cf33134d4b5fc4490
Bartlomiej <guminiak.bartlomiej@gmail.com>
2019-06-01 00:04:53 +0200

    Add test

--- TDDLottery/TDDLotteryTests/Scenes/Lottery/LotteryViewControllerTests.swift
+++ TDDLottery/TDDLotteryTests/Scenes/Lottery/LotteryViewControllerTests.swift
@@ -33,4 +33,19 @@ class LotteryViewControllerTests: XCTestCase {
         XCTAssertEqual(customView.stackView.arrangedSubviews.count, 3)
     }
 
+    func test_whenViewLoaded_circleViewModelsProvided_shouldhaveCorrectData() {
+        sut.viewDidLoad()
+        let viewModel1 = CircleViewModelStub()
+        viewModel1.title = "test-1"
+        viewModel1.color = .black
+        let viewModel2 = CircleViewModelStub()
+        viewModel2.title = "test-2"
+        viewModel2.color = .white
+        viewModelDouble.circleViewModels?([viewModel1, viewModel2])
+        let customView = sut.view as! LotteryView
+        let labels: [UILabel] = customView.stackView.arrangedSubviews.compactMap { $0 as? UILabel }
+        XCTAssertEqual(labels.map { $0.text }, ["test-1", "test-2"])
+        XCTAssertEqual(labels.map { $0.backgroundColor }, [.black, .white])
+    }
+
 }
