4547805285aff2877a1767f1af453eef555212d8
Bartlomiej <guminiak.bartlomiej@gmail.com>
2019-06-09 16:27:58 +0200

    Implemented visual components

--- TDDLottery/TDDLottery/Scenes/Lottery/LotterView.swift
+++ TDDLottery/TDDLottery/Scenes/Lottery/LotterView.swift
@@ -19,7 +19,16 @@ class LotteryView: UIView {
     }
 
     private func setupSubviews() {
+        backgroundColor = .white
+        stackView.alignment = .center
+        stackView.distribution = .equalCentering
         stackView.translatesAutoresizingMaskIntoConstraints = false
+        let contstraints = [
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            stackView.topAnchor.constraint(equalTo: topAnchor, constant: 100)
+        ]
+        NSLayoutConstraint.activate(contstraints)
     }
 
 }
--- TDDLottery/TDDLottery/Scenes/Lottery/LotteryViewController.swift
+++ TDDLottery/TDDLottery/Scenes/Lottery/LotteryViewController.swift
@@ -27,15 +27,32 @@ class LotteryViewController: UIViewController {
 
     private func configureSubviews() {
         viewModel.circleViewModels = { [unowned self] viewModels in
-            let labels = viewModels.map { viewModel -> UILabel in
-                let label = UILabel()
-                label.text = viewModel.title
-                label.backgroundColor = viewModel.color
-                return label
-            }
+            self.lotteryView.stackView.subviews.forEach(self.lotteryView.stackView.removeArrangedSubview)
+            let labels = viewModels.map(self.createLabel)
             labels.forEach(self.lotteryView.stackView.addArrangedSubview)
+            labels.forEach(self.setupSizeConstraints)
         }
         viewModel.viewDidLoad()
+    }
+
+    private func createLabel(with viewModel: CircleViewModelType) -> UILabel {
+        let label = UILabel()
+        label.textColor = .white
+        label.text = viewModel.title
+        label.backgroundColor = viewModel.color
+        label.textAlignment = .center
+        label.clipsToBounds = true
+        label.layer.cornerRadius = 30
+        return label
+    }
+
+    private func setupSizeConstraints(_ view: UILabel) {
+        view.translatesAutoresizingMaskIntoConstraints = false
+        let constraints = [
+            view.heightAnchor.constraint(equalToConstant: 60),
+            view.widthAnchor.constraint(equalToConstant: 60)
+        ]
+        NSLayoutConstraint.activate(constraints)
     }
 
 }
