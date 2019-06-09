0d69ba034067398bc07acc1126c5673408dbd47f
Bartlomiej <guminiak.bartlomiej@gmail.com>
2019-06-09 18:47:02 +0200

    Add titleLabel

--- TDDLottery/TDDLottery/Scenes/Lottery/LotterView.swift
+++ TDDLottery/TDDLottery/Scenes/Lottery/LotterView.swift
@@ -10,23 +10,31 @@ class LotteryView: UIView {
 
     required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
 
+    let titleLabel = UILabel()
     let stackView = UIStackView(frame: .zero)
 
     // MARK: - Privates
 
     private func addSubviews() {
+        addSubview(titleLabel)
         addSubview(stackView)
     }
 
     private func setupSubviews() {
         backgroundColor = .white
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+
+        titleLabel.textAlignment = .center
         stackView.alignment = .center
         stackView.distribution = .equalCentering
-        stackView.translatesAutoresizingMaskIntoConstraints = false
+
         let contstraints = [
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 40),
+            titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            stackView.topAnchor.constraint(equalTo: titleLabel.topAnchor, constant: 100),
             stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
-            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
-            stackView.topAnchor.constraint(equalTo: topAnchor, constant: 100)
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20)
         ]
         NSLayoutConstraint.activate(contstraints)
     }
