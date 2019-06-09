f2192463d99bb084b2233461562450ee163deb11
Bartlomiej <guminiak.bartlomiej@gmail.com>
2019-06-09 18:32:28 +0200

    Add logic

--- TDDLottery/TDDLottery/Scenes/Lottery/FormattedDateProvider.swift
+++ TDDLottery/TDDLottery/Scenes/Lottery/FormattedDateProvider.swift
@@ -10,17 +10,22 @@ class FormattedDateProvider: FormattedDateProviderType {
         currentDate: @escaping () -> Date) {
         self.dateFormatter = dateFormatter
         self.currentDate = currentDate
+        configureDateFormatter()
     }
 
     // MARK: - FormattedDateProviderType
 
     func stringDate() -> String {
-        fatalError()
+        return dateFormatter.string(from: currentDate())
     }
 
     // MARK: - Privates
 
     private let dateFormatter: DateFormatter
     private let currentDate: () -> Date
+
+    private func configureDateFormatter() {
+        dateFormatter.dateFormat = "d/M/YYYY"
+    }
 
 }
